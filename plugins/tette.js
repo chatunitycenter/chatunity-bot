aab14d3f900bef44ea852d22216ca9cd22053959
BLOOD212 <g95829914@gmail.com>
2026-02-13 16:17:23 +0100

    Update tette.js

--- plugins/tette.js
+++ plugins/tette.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn }) =>  
+let handler = async (m, { conn }) => {
   let user = m.mentionedJid && m.mentionedJid[0]
     ? m.mentionedJid[0]
     : m.quoted
@@ -9,30 +9,37 @@ let handler = async (m, { conn }) =>
     return m.reply("Devi menzionare qualcuno 😏");
   }
 
-  // Numeri possibili
-  const numeri = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const numeri = [1,2,3,4,5,6,7,8,9];
+  const lettere = ["A","B","C","D","E","F"];
 
-  // Lettere possibili
-  const lettere = ["A", "B", "C", "D", "E", "F"];
-
-  // Random
   const numeroRandom = numeri[Math.floor(Math.random() * numeri.length)];
   const letteraRandom = lettere[Math.floor(Math.random() * lettere.length)];
-
   const misura = `${numeroRandom}${letteraRandom}`;
 
-  let testo = `oh @${user.split("@")[0]} ha una ${misura}`;
+  const fortuna = Math.floor(Math.random() * 101); // 0-100%
+
+  const frasi = [
+    `oh @${user.split("@")[0]} ha una ${misura} 😳`,
+    `attenzione ⚠️ @${user.split("@")[0]} possiede una ${misura}`,
+    `le statistiche parlano chiaro 📊 @${user.split("@")[0]} ha una ${misura}`,
+    `gli esperti confermano 🧪 @${user.split("@")[0]} ha una ${misura}`,
+    `breaking news 📰 @${user.split("@")[0]} ha una ${misura}`
+  ];
+
+  const fraseRandom = frasi[Math.floor(Math.random() * frasi.length)];
+
+  let testoFinale = `${fraseRandom}\n🍀 Fortuna: ${fortuna}%`;
 
   await conn.sendMessage(
     m.chat,
     {
-      text: testo,
+      text: testoFinale,
       mentions: [user],
     },
     { quoted: m }
   );
 };
 
-handler.command = ["tette"]; // <-- comando in fondo
+handler.command = ["tette"]; // comando in fondo
 
 module.exports = handler;
